26214381c458a418423bab44f539e2976569c19a
IgorMontezuma20 <igorminranda17@gmail.com>
2023-07-22 23:04:48 -0300

    feat: implementando a validacao de cpf e endereco

--- src/pages/Payment/validationSchema.tsx
+++ src/pages/Payment/validationSchema.tsx
@@ -1,21 +1,53 @@
+import { isValidCNPJ, isValidCPF, isValidPhone } from '@brazilian-utils/brazilian-utils'
 import * as yup from 'yup'
-
-import { isValidPhone } from '@brazilian-utils/brazilian-utils'
 
 export const schema = yup
   .object({
     fullName: yup
       .string()
-      .required('Nome e sobrenome são obrigatórios.')
-      .min(3, 'Nome e sobrenome muito curto')
-      .matches(/(\w.+\s).+/gi, 'O nome deve conter o sobrenome'),
+      .required('O nome é obrigatório.')
+      .min(3, 'O nome deve ser completo.')
+      .matches(/(\w.+\s).+/gi, 'O nome deve conter o sobrenome.'),
     email: yup.string().required('O email é obrigatório.').email('O email deve ser válido.'),
     mobile: yup
       .string()
       .required('O celular é obrigatório.')
       .transform((value) => value.replace(/[^\d]/g, ''))
       .test('validateMobile', 'O celular inválido.', (value) => isValidPhone(value)),
+    document: yup
+      .string()
+      .required('O CPF/CNPJ é obrigatório.')
+      .transform((value) => value.replace(/[^\d]/g, ''))
+      .test(
+        'validateDocument',
+        'O CPF/CNPJ é inválido.',
+        (value) => isValidCPF(value) || isValidCNPJ(value),
+      ),
+    zipCode: yup
+      .string()
+      .required('O CEP é obrigatório.')
+      .transform((val) => val.replace(/[^\d]+/g, '')),
+    street: yup.string().required('O endereço é obrigatório.'),
+    number: yup.string().required('O número é obrigatório.'),
+    complement: yup.string(),
+    neighborhood: yup.string().required('O bairro é obrigatório.'),
+    city: yup.string().required('A cidade é obrigatória.'),
+    state: yup.string().required('O estado é obrigatório.'),
   })
   .required()
 
 export type FieldValues = yup.InferType<typeof schema>
+//export type FieldValues = {
+//  fullName: string,
+//  email: string,
+//  mobile: string,
+//  document: string,
+//  zipCode: string,
+//  street: string,
+//  state: string,
+//  city: string,
+//  number: string,
+//  neighborhood: string,
+//  complement?: string,
+//
+//}
