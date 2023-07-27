c642345caa81cee323fa681bf712b4894e8c080a
IgorMontezuma20 <igorminranda17@gmail.com>
2023-07-27 10:56:27 -0300

    feat: ajustando as chamadas dos endpoints

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,9 +5,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
-export const getBurgers = () => api.get<snackData[]>('/burgers')
-export const getPizzas = () => api.get<snackData[]>('pizzas')
-export const getDrinks = () => api.get<snackData[]>('/drinks')
-export const getDesserts = () => api.get<snackData[]>('/desserts')
+export const getBurgers = () => api.get<snackData[]>('/snacks?snack=burger')
+export const getPizzas = () => api.get<snackData[]>('snacks?snack=pizza')
+export const getDrinks = () => api.get<snackData[]>('snacks?snack=drink')
+export const getDesserts = () => api.get<snackData[]>('snacks?snack=ice-cream')
 
 export default api
